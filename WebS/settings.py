0b5343c1b5810561c20698c6f34ab5534e96a6fc
DenL <den.litviniuk@gmail.com>
2023-12-24 13:33:03 +0200

    initialized restapi app + gitignored db file

--- WebS/settings.py
+++ WebS/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'WebS.apps.authentication',
     'WebS.apps.main',
+    'WebS.apps.restapi',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- WebS/urls.py
+++ WebS/urls.py
@@ -2,11 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import WebS.apps.main.urls
-import WebS.apps.authentication.urls
+import WebS.apps.main.urls as main_urls
+import WebS.apps.authentication.urls as auth_urls
+import WebS.apps.restapi.urls as rest_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(WebS.apps.main.urls.urlpatterns)),
-    path('', include(WebS.apps.authentication.urls.urlpatterns)),
+    path('', include(main_urls)),
+    path('', include(auth_urls)),
+    path('', include(rest_urls)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
